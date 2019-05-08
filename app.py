d91524472e01d077b2b9a2e9aa38b8513f8151c5
James <jfarrant4@gmail.com>
2019-05-08 21:58:33 +0100

    adds flask endpoint scaffold functions

--- app.py
+++ app.py
@@ -4,5 +4,15 @@ from flask import Flask
 app = Flask()
 
 
+@app.route("/predict/csv", methods=["POST"])
+def csv_customer_likelihood(csv):
+    pass
+
+
+@app.route("predict/customer", methods=["POST"])
+def json_customer_likelihood(customer_profile):
+    pass
+
+
 if __name__ == "__main__":
     app.run("localhost")
