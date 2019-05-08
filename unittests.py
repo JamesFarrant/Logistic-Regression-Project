da9e88c365c988328acb1180a84b4e4693365546
James <jfarrant4@gmail.com>
2019-05-08 21:53:47 +0100

    adds invalid delimiter test

--- unittests.py
+++ unittests.py
@@ -3,6 +3,8 @@ import pandas as pd
 from .model import load_data
 
 
+# ----- load_data tests -----
+
 def test_load_data_as_frame():
     valid_file = "data/bank-full.csv"
     assert type(load_data(valid_file)) == pd.DataFrame
@@ -12,3 +14,11 @@ def test_load_data_file_not_found():
     with pytest.raises(FileNotFoundError):
         load_data("bank.csv")
 
+
+def test_load_data_invalid_delimeter():
+        with pytest.raises(TypeError):
+                load_data("data/bank-full.csv", 1.0203)
+                load_data("data/bank-full.csv", 123)
+                load_data("data/bank-full.csv", [])
+                load_data("data/bank-full.csv", {})
+                load_data("data/bank-full.csv", (1, 2, 3))
