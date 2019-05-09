49af1ac89e8c00b46f3d4a760f6e678f4f520933
James Farrant <jamesf@diabetes.co.uk>
2019-05-09 14:55:42 +0100

    adds TODOs and prototype JSON user

--- predict.py
+++ predict.py
@@ -1,18 +1,56 @@
 # TODO: Create an API endpoint which accepts relevant parameters from a
 # potential customer and returns the likelihood of them
 # also subscribing to the product
-from model import preprocess_training_data
+from model import load_data, preprocess_training_data
 import pandas as pd
 import joblib
+import json
 
 
+# TODO: Data formatting when submitted as JSON
+# This particular profile _should_ have a high probability of 0
+# (not subscribing)
+
+# TODO: Preprocess input JSON to be the same OHE format as the trained model
+# ahead of prediction
+# simply retrieve (and store, if needed) the list of columns after the training OHE output. 
+# Then run pd.get_dummies on the test set/example. Loop through the output test OHE columns, 
+# drop those that do not appear in the training OHE and add those that are missing in test OHE filled with zeros.
+test_user = json.dumps(
+    {
+        "age": 58,
+        "job": "management",
+        "education": "tertiary",
+        "default": "no",
+        "balance": 2143,
+        "housing": "yes",
+        "loan": "no",
+        "contact": "unknown",
+        "day": 5,
+        "month": "may",
+        "duration": 261,
+        "campgaign": 1,
+        "pdays": -1,
+        "previous": 0,
+        "poutcome": "unknown",
+    }
+)
+
+user = pd.read_csv("data/bank-full.csv")
+
+'''
 def create_user_profile(user):
     """[summary]
 
     Arguments:
         user {[type]} -- [description]
     """
-    pass
+    user = pd.read_json(user, )
+    return user
+
+
+print(create_user_profile(test_user))
+'''
 
 
 def predict_user(
@@ -25,3 +63,6 @@ def predict_user(
     """
     model = joblib.load(model)
     prediction = model.predict_proba(user_profile)
+
+
+print(predict_user(pd.get_dummies(user)))
