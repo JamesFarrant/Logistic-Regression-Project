a8e501e501e7610fce84604571214836465acf5e
James Farrant <jamesf@diabetes.co.uk>
2019-05-09 12:22:29 +0100

    adds predict_user scaffold function

--- predict.py
+++ predict.py
@@ -1,3 +1,11 @@
 # TODO: Create an API endpoint which accepts relevant parameters from a
 # potential customer and returns the likelihood of them
 # also subscribing to the product
+
+def predict_user(user_profile):
+    """[summary]
+    
+    Arguments:
+        user_profile {[type]} -- [description]
+    """
+    pass
