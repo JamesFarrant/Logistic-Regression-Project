a83145afdb0e9553ef01764083b92dd1149781a5
James Farrant <jamesf@diabetes.co.uk>
2019-05-10 15:23:00 +0100

    changes app.py route slightly

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, Response, request
+from predict import create_user_profile, predict_user
 
-
-app = Flask()
+app = Flask(__name__)
 
 
 @app.route("/predict/csv", methods=["POST"])
@@ -9,9 +9,10 @@ def csv_customer_likelihood(csv):
     pass
 
 
-@app.route("predict/customer", methods=["POST"])
-def json_customer_likelihood(customer_profile):
-    pass
+@app.route("/predict/customer", methods=["POST"])
+def json_customer_likelihood():
+    customer_profile = request.get_json()
+    return customer_profile
 
 
 if __name__ == "__main__":
