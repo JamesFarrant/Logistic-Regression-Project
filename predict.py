b8a85062f8bc7b4354ddc49eb7cda3f6331cf47c
James <jfarrant4@gmail.com>
2019-05-12 21:59:03 +0100

    adds docstring/explanation for predict_frame

--- predict.py
+++ predict.py
@@ -7,10 +7,13 @@ import json
 
 
 def create_user_profile(user: dict) -> pd.DataFrame:
-    """[summary]
+    """Returns a pd.DataFrame representation of a user from a JSON object.
 
     Arguments:
-        user {[type]} -- [description]
+        user {dict} -- JSON object posted via the /predict/customer route.
+    
+    Returns:
+        user {pd.DataFrame} -- User profile for further analysis.
     """
     user = pd.DataFrame.from_dict([user])
     return user
@@ -44,10 +47,33 @@ def predict_user(user_profile: pd.DataFrame, round_prec=3) -> float:
 
 
 def predict_frame(frame: pd.DataFrame) -> str:
-    """
+    """Takes in a pd.DataFrame from an uploaded .csv file.
 
-    :param frame:
-    :return: str
+    [For the purposes of the exercise] -- Drops the Y column for predictions.
+
+    Reindexes the DataFrame according to a one-hot encoded representation of
+    all of the columns.
+
+    Applies the loaded logistic regression model across the reshaped numpy
+    array representation of each row, creating a new pd.Series column in the
+    process. Rounds the values for nicer viewing.
+
+    ---
+    The reason why reindex_frame is used for the application and is not 
+    returned is to reduce the load on to_json() when it returns the resulting
+    JSON string from the predictions (instead of returning all of the OHE cols
+    as well).
+    ---
+
+    Finally, returns the pd.DataFrame as a JSON string back to the /predict/csv
+    API route.
+    
+    Arguments:
+        frame {pd.DataFrame} -- [description]
+    
+    Returns:
+        str -- Record-oriented JSON string containing all original columns
+
     """
     model = joblib.load("models/LogReg.pkl")
     frame = load_data(frame)
